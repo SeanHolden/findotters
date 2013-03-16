0635dfa9731c11179c4c3153b12f41a4ae5d0398
SeanHolden <sean.holden@o2.com>
2013-03-16 18:13:33 +0000

    editing migration to solve postgres issues

--- db/migrate/20130316175705_convert_lat_and_long_to_floats.rb
+++ db/migrate/20130316175705_convert_lat_and_long_to_floats.rb
@@ -1,11 +1,21 @@
 class ConvertLatAndLongToFloats < ActiveRecord::Migration
   def up
-    change_column :otter_surveys, :lat, :float
-    change_column :otter_surveys, :long, :float
+    # change_column :otter_surveys, :lat, :float
+    # change_column :otter_surveys, :long, :float
+    rename_column :otter_surveys, :lat, :lat_string
+    rename_column :otter_surveys, :long, :long_string
+    add_column :otter_surveys, :lat, :float
+    add_column :otter_surveys, :long, :float
+    remove_column :otter_surveys, :lat_string
+    remove_column :otter_surveys, :long_string
   end
 
   def down
-    change_column :otter_surveys, :lat, :string
-    change_column :otter_surveys, :long, :string
+    rename_column :otter_surveys, :lat, :lat_float
+    rename_column :otter_surveys, :long, :long_float
+    add_column :otter_surveys, :lat, :string
+    add_column :otter_surveys, :long, :string
+    remove_column :otter_surveys, :lat_float
+    remove_column :otter_surveys, :long_float
   end
 end
