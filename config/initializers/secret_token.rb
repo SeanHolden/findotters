125cc490c93860b70dd61d4f5b11ca6f8f498b3d
SeanHolden <sean.holden@o2.com>
2013-03-16 13:24:32 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindOtter::Application.config.secret_token = '8dc5486951a2111453583566f202a5669349a5adedfb0e7b7d7882e56ba411524ae0a6ecc4e56e50c2d227fafb50be6519188c46f266d97da508e99c3e1b0efc'
